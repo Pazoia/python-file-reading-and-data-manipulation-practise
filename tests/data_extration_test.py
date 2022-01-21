ab43b671bceb4d9a00d28b58c8564acbddc4d7eb
Paulo Azoia <pazoia.gomes@gmail.com>
2022-01-21 17:51:03 +0000

    change test to check for expected data and if filepath was called

--- tests/data_extration_test.py
+++ tests/data_extration_test.py
@@ -5,9 +5,9 @@ from unittest.mock import mock_open, patch
 def test_get_data_from_file_returns_data():
     data_extraction = DataExtraction()
     
-    mocked_data = "Some data returned"
+    m = mock_open(read_data="Some data returned")
 
-    with patch("builtins.open", mock_open(read_data=mocked_data)):
-        result = data_extraction.get_data_from_file("test_filename")
-    
-    assert result == "Some data returned"
+    with patch("builtins.open", m) as mocked_open:
+        actual = data_extraction.get_data_from_file("fake/file/path")
+        assert actual == "Some data returned"
+        mocked_open.assert_called_once_with("fake/file/path", "r")
